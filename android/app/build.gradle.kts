65ae8d2e720f584b0c5b74b9e52b39d2ff0dbb05
admin <admin@admindeMac-mini.local>
2026-01-14 00:13:18 -0800

    chore: 调整Android打包配置
    注释掉x86架构排除配置，支持更多设备架构

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,12 +19,12 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
-    packagingOptions {
-        excludes += setOf(
-            "lib/x86/**",
-            "lib/x86_64/**"
-        )
-    }
+//    packagingOptions {
+//        excludes += setOf(
+//            "lib/x86/**",
+//            "lib/x86_64/**"
+//        )
+//    }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
